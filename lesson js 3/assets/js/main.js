badd453237865572829773066f43f60bc3f33e09
damirpopo <43522476+damirpopo@users.noreply.github.com>
2022-10-28 11:44:14 +0300

    Add files via upload

---
+++ lesson js 3/assets/js/main.js
@@ -0,0 +1,104 @@
+// let num=1;
+// while (num<101) {
+//     console.log(num);
+//     num++;
+// }
+
+
+// let num=11;
+// while (num<34) {
+//     console.log(num);
+//     num++;
+// }
+
+// let num=0;
+// while (num<101) {
+//     console.log(num);
+//     num+=2;
+// }
+
+
+// let num=1;
+// let sum=0
+// while (num<101) {
+//     sum+=num;
+//     num++;
+// }
+// alert(sum)
+
+
+
+// let x=1
+// for (x;x<6;x++){
+//     alert(x)
+// }
+
+
+// let mas=[1,2,3,4,5];
+// let result=0
+// let x=0
+// for (x;x<mas.length;x++){
+//     result=+mas[x]
+// }
+// alert(result)
+
+
+// var obj={green: 'зелёный', red: 'красный', blue: 'синий'};
+// for (let i in obj) {
+//     console.log(`${i}: ${obj[i]}`);
+//   }
+
+// var obj={коля: '200', вася: '300', петя: '400'};
+// for (let i in obj) {
+//     console.log(`${i}- зарплата ${obj[i]} долоров`);
+//   }
+
+
+// mas=[2,5,9,15,0,4]
+// for (let i = 0; i < mas.length; i++) {
+//     if (mas[i] > 3 && mas[i] < 10) 
+//     alert(mas[i]);
+//     }
+
+
+
+// mas=[2,-5,9,-15,0,4]
+// let sum=0
+// for (let i = 0; i < mas.length; i++) {
+//     if (mas[i] > 0 )
+//     sum+=mas[i];
+//     alert(sum);
+//     }
+
+
+// mas=[1, 2, 5, 9, 4, 13, 4, 10]
+// for (let i = 0; i < mas.length; i++) {
+//     if (mas[i]==4) 
+//     alert('есть');
+//     }
+
+// mas=[1,2,3,4,5,6,7,8,9]
+// for (let i in mas) {
+//      console.log(`-${mas[0]}-${mas[1]}-${mas[2]}-${mas[3]}-${mas[4]}-${mas[5]}-${mas[6]}-${mas[7]}-${mas[8]}-`);
+// }
+
+// mas=['пн','вт','ср','чт','пт','сб','вс']
+// for (let i in mas) {
+//       console.log(`${mas[0]},${mas[1]},${mas[2]},${mas[3]},${mas[4]},${mas[5]},${mas[6]}`);
+//  }
+
+// mas=['пн','вт','ср','чт','пт','сб','вс']
+// for (let i in mas) {
+//       console.log(`${mas[0]},${mas[1]},${mas[2]},${mas[3]},day${mas[4]},${mas[5]},${mas[6]}`);
+//  }
+
+// let n=1000
+// let num=0
+// while (n>50){
+//     n/=2
+//     num++
+    
+// }
+// alert(num)
+
+
